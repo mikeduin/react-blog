e77de16ef4347648048a4af67597260bce47318e
Mike Duin <michael.s.duin@gmail.com>
2019-01-22 00:17:46 -0800

    shift find-user logic to mapStateToProps

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -8,7 +8,8 @@ class UserHeader extends React.Component {
   }
 
   render () {
-    const user = this.props.users.find((user) => user.id === this.props.userId);
+    // const user = this.props.users.find((user) => user.id === this.props.userId);
+    const { user } = this.props;
 
     if (!user) {
       return null;
@@ -18,9 +19,9 @@ class UserHeader extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
   return {
-    users: state.users
+    user: state.users.find(user => user.id === ownProps.userId)
   };
 }
 
