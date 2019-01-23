dfbfd7aa717515493436bb16d2136040113cd5ff
Mike Duin <michael.s.duin@gmail.com>
2019-01-22 18:06:31 -0800

    restructure for alternate overfetching

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,15 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => {
+  return async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+  }
+}
+
 export const fetchPosts = () => {
   return async (dispatch) => {
     const response = await jsonPlaceholder.get('/posts');
@@ -9,12 +18,23 @@ export const fetchPosts = () => {
   };
 };
 
-export const fetchUser = (id) => dispatch => {
-  _fetchUser(id, dispatch);
-};
+// Note that the functions above and below this comment are identical in terms of structure -- the syntax below is just shortened via ES6 conventions
 
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: 'FETCH_USER', payload: response.data})
-});
+};
+
+
+// MEMOIZED APPROACH BELOW
+
+// export const fetchUser = (id) => dispatch => {
+//   _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//
+//   dispatch({ type: 'FETCH_USER', payload: response.data})
+// });
